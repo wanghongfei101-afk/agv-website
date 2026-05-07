beb5d7e249581b11d8dae7bb362dcfc0939a4598
zw99154-hash <zw99154@gmail.com>
2026-05-07 23:00:01 +0800

    稳定版本备份：修复了布局和间距

---
+++ react-pageflip-master/rollup.config.js
@@ -0,0 +1,30 @@
+import autoprefixer from 'autoprefixer';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    external: ['react', 'react-dom', 'page-flip'],
+    input: 'src/index.ts',
+    watch: {
+        include: 'src/**',
+    },
+    output: [
+        {
+            file: 'build/index.js',
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: 'build/index.es.js',
+            format: 'esm',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        typescript({ useTsconfigDeclarationDir: true }),
+        postcss({
+            plugins: [autoprefixer()],
+            minimize: true,
+        }),
+    ],
+};
